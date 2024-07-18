6c1b5533bbe4b426307ace3bcb85c45b1c360341
Wanderer0074348 <tanaymatta27@gmail.com>
2024-07-18 15:34:28 +0400

    Completed dynamic generation of questions

--- practice_app_2/lib/answer_button.dart
+++ practice_app_2/lib/answer_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AnswerButton extends StatelessWidget {
   const AnswerButton(
@@ -9,14 +10,18 @@ class AnswerButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: onPress,
       style: ElevatedButton.styleFrom(
-          padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
+          // padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
           backgroundColor: const Color.fromARGB(255, 35, 29, 118),
           foregroundColor: const Color.fromARGB(255, 187, 180, 232),
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
-      child: Text(answerText),
+      child: Text(
+        answerText,
+        textAlign: TextAlign.center,
+        style: GoogleFonts.ubuntuCondensed(fontSize: 18),
+      ),
     );
   }
 }
--- practice_app_2/lib/questions_screen.dart
+++ practice_app_2/lib/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:practice_app_2/answer_button.dart';
 import 'package:practice_app_2/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,26 +12,46 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(
+      () {
+        currentQuestionIndex++;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(color: Colors.white, fontSize: 18),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          AnswerButton(answerText: currentQuestion.answers[0], onPress: () {}),
-          AnswerButton(answerText: currentQuestion.answers[1], onPress: () {}),
-          AnswerButton(answerText: currentQuestion.answers[2], onPress: () {}),
-          AnswerButton(answerText: currentQuestion.answers[3], onPress: () {})
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(80),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              textAlign: TextAlign.center,
+              currentQuestion.text,
+              style: GoogleFonts.ubuntu(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            // spreading the list data to a widget of all the elements
+            ...currentQuestion.getShuffled().map(
+              (item) {
+                return AnswerButton(onPress: answerQuestion, answerText: item);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
